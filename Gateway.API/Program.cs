cfa0178479b010eb0e60a01de4ed018331debbe2
Abhay Tiwari <abhay3032003@gmail.com>
2026-06-23 10:24:45 +0530

    Implemented API Gateway with JWT secured microservices

--- Gateway.API/Program.cs
+++ Gateway.API/Program.cs
@@ -1,20 +1,68 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Yarp.ReverseProxy;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Reverse Proxy
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(
         builder.Configuration.GetSection("ReverseProxy"));
 
+// JWT Authentication
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme =
+            JwtBearerDefaults.AuthenticationScheme;
+
+        options.DefaultChallengeScheme =
+            JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer =
+                    builder.Configuration["Jwt:Issuer"],
+
+                ValidAudience =
+                    builder.Configuration["Jwt:Audience"],
+
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(
+                            builder.Configuration["Jwt:Key"]!
+                        )
+                    )
+            };
+    });
+
+builder.Services.AddAuthorization();
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Authentication & Authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Reverse Proxy
 app.MapReverseProxy();
 
 app.Run();
